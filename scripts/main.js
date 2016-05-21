589801a8d0ee581094b71ed1cce25cc8cb08a302
George Francis <georgefrancis105@gmail.com>
2016-05-21 15:10:14 +1200

    added basic styling

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,16 @@
 
 
-$("#button-twitter").hide();
-
-
-
 $(document).ready(function(){
+  //$("#button-twitter").hide();
   console.log("doc ready");
   newQuote();
   $("#button-new-quote").click(function(){
     console.log("button-new-quote PUSHED");
     newQuote();
+  });
+  $("#button-twitter").click(function(){
+    console.log("button-twitter PUSHED");
+    alert("button does nothing, not finished yet");
   });
 });
 
@@ -24,5 +25,5 @@ function newQuote() {
 function mycallback(result) {
     console.log(result);
     $("#quote-text").html(result[0].content);
-    $("#quote-title").html(result[0].title);
+    $("#quote-title").html("- "+result[0].title);
 }
