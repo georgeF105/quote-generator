329af96a48c29f1af26714233ef0d175091198ad
George Francis <georgefrancis105@gmail.com>
2016-05-21 14:46:15 +1200

    basic functionality added

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,28 @@
 
+
+$("#button-twitter").hide();
+
+
+
+$(document).ready(function(){
+  console.log("doc ready");
+  newQuote();
+  $("#button-new-quote").click(function(){
+    console.log("button-new-quote PUSHED");
+    newQuote();
+  });
+});
+
+function newQuote() {
+  $.ajax( {
+        url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=mycallback',
+        dataType: 'jsonp'
+        
+  });
+}
+
+function mycallback(result) {
+    console.log(result);
+    $("#quote-text").html(result[0].content);
+    $("#quote-title").html(result[0].title);
+}
